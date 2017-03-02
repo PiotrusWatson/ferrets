ba01aaeb2f4542f5f2e9b72d2586bcd9dd04be83
KerryJohnstone <2189152j@student.gla.ac.uk>
2017-03-02 14:03:46 +0000

    Modified models.py

--- ferrets/categories/models.py
+++ ferrets/categories/models.py
@@ -1,5 +1,42 @@
 from __future__ import unicode_literals
-
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Category)
+    user = models.ForeignKey(User)
+    item_name = models.CharField(max_length=128, blank=True)
+    price = models.DecimalField(max_digits=None, decimal_places=2)
+    description = models.TextField(max_length=350, blank=True)
+    picture = models.ImageField(upload_to='item_images', blank=True)
+    views = models.IntegerField(default=0)
+    date_added = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+class User(models.Model):
+    user = models.OneToOneField(User)
+    email = models.EmailField(max_length=254, blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User)
+    item = models.ForeignKey(Item)
+
