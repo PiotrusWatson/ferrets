028299eb6c23d58219a4f3d29dd8f22785bd3a0f
Piotrus Watson <piotruswatson@gmail.com>
2017-03-02 13:18:56 +0000

    appname more accurate, site still works

--- ferrets/ferrets/urls.py
+++ ferrets/ferrets/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from ferreted_away import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
