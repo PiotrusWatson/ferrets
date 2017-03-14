3b107027917b8422ddf23095bdf8b8771f7f8495
Domenico Restucia <2201547R@student.gla.ac.uk>
2017-03-14 12:00:28 +0000

    updated login, home

--- ferrets/ferreted_away/views.py
+++ ferrets/ferreted_away/views.py
@@ -1,21 +1,30 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from ferreted_away.models import Category, Item, User, Watchlist
 from django.contrib.auth.decorators import login_required
-from ferreted_away.models import Item, Watchlist, Category
+from datetime import datetime
 
 def home(request):
-    item_list = Item.objects.order_by("-views")[:3]
+    item_list = Item.objects.order_by('-views')[:5]
 
-    context_dict = {'items': item_list}
-    return render(request, 'ferrets/index.html', context_dict)
+
+    context_dict = {'items': item_list,
+                    }
+
+    if request.user.is_authenticated():
+        watched_list = Watchlist.filter(user=request.user.user).order_by("-date_added")[:5]
+        context_dict = {'items': item_list,
+                        'watched': watched_list,
+                        }
+
+    return render(request, 'ferrets/home.html', context=context_dict)
 
 
 def about(request):
     context_dict = {'stuff':'place holder stuff'}
     return render(request, 'ferrets/about.html', context_dict)
-
-
 
 def faq(request):
     return render(request, "ferrets/faq.html")
@@ -28,12 +37,31 @@ def sitemap(request):
 
 
 def login(request):
+
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse("Your Rango account is disabled")
+
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+
+            return render(request, 'ferrets/login.html',{'message' :"Invalid Username or Password"})
+
     return render(request, "ferrets/login.html")
 
 def addAccount(request):
     return render(request, "ferrets/addaccount.html")
 
-#@login_required
+@login_required
 def myAccount(request):
     my_items = Item.objects.filter(user=request.user.user).order_by("-date_added")[:5]
     my_watchlist = Watchlist.filter(user=request.user.user).order_by("-date_added")[:5]
@@ -41,26 +69,26 @@ def myAccount(request):
     context_dict = {"my_items":my_items, "my_watchlist":my_watchlist}
     return render(request, "ferrets/myaccount.html", context_dict)
 
-#@login_required
+@login_required
 def myItems(request):
     items = Item.objects.filter(user=request.user.user)
 
     context_dict = {"items": items}
     return render(request, "ferrets/myitems.html", context_dict)
 
-#@login_required
+@login_required
 def addItems(request):
     return render(request, "ferrets/additems.html")
 
-#@login_required
+@login_required
 def myWatchlist(request):
-    watchlist = Watchlist.filter(user=request.user.user)
+    watchlist = Watchlist.filter(user=request.user.user).order_by("-date_added")[:5]
 
     context_dict = {"watchlist": watchlist}
     return render(request, "ferrets/mywatchlist.html", context_dict)
 
 def categories(request):
-    categories = Category.objects.all()
+    categories = Category.objects
 
     context_dict ={"categories": categories}
 
