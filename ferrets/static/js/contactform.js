3f98681ff40583e46e350907921c366f8ea493a1
KerryJohnstone <2189152j@student.gla.ac.uk>
2017-03-16 17:02:41 +0000

    Edited email message and modified my account page

--- ferrets/static/js/contactform.js
+++ ferrets/static/js/contactform.js
@@ -7,25 +7,10 @@ $("#sendButton").click(function() {
 		)
 
         .done(function(response) {
-			alert("Done");
-            // $(formMessages).removeClass('error');
-            // $(formMessages).addClass('success');
-
-            // $(formMessages).text(response);
-
-            // $('#name').val('');
-            // $('#email').val('');
-            // $('#message').val('');
+			$(".message").append("Your email was sent successfuly. Our team will be in touch!");
         })
 
         .fail(function(data) {
-            // $(formMessages).removeClass('success');
-            // $(formMessages).addClass('error');
-
-            // if (data.responseText !== '') {
-                // $(formMessages).text(data.responseText);
-            // } else {
-                // $(formMessages).text('Sorry! Soemthing went wrong. Message not sent.');
-            // }
+            $(".message").append("Your message failed to send. Please try again later.");
         });
 });
