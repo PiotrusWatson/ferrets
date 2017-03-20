e151588ac7a459be92f03e2b34ad00fd8144f699
KerryJohnstone <2189152j@student.gla.ac.uk>
2017-03-20 14:20:59 +0000

    Made chnages to search results but wont know if its worked until we get it up on python anywhere

---
+++ ferrets/static/js/search.js
@@ -0,0 +1,9 @@
+ (function() {
+   var cx = '001784674130566875644:_toxp1nrbns';
+   var gcse = document.createElement('script');
+   gcse.type = 'text/javascript';
+   gcse.async = true;
+   gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(gcse, s);
+ })();
