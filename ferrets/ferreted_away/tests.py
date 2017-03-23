96c754d292dcd4fb93bc1cd4161cf7c1d28249b0
KerryJohnstone <2189152j@student.gla.ac.uk>
2017-03-23 19:16:21 +0000

    Our first working test case

--- ferrets/ferreted_away/tests.py
+++ ferrets/ferreted_away/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from ferreted_away.models import *
+"""
+https://github.com/leifos/tango_with_django_19/blob/master/code/tango_with_django_project/rango/tests.py
+"""
 
-# Create your tests here.
+class ModelTests(TestCase):
+	def setUp(self):
+		try:
+			from population_script import populate
+			populate()
+		except ImportError:
+			print('The module population_script does not exist')
+		except NameError:
+			print('The function populate() does not exist')
+		except:
+			print('Undisclosed error')
+
+	def get_category(self, name):
+		from ferreted_away.models import Category
+		try:
+			cat = Category.objects.get(name=name)
+		except Category.DoesNotExist:
+			cat = None
+		return cat
+		
+	def test_if_category_added(self):
+		cat = self.get_category('Transport')
+		self.assertIsNotNone(cat)
