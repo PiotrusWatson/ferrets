b96d8674ad817ba1e87f32125b5cbf8deb501da1
Piotrus Watson <piotruswatson@gmail.com>
2017-03-23 11:03:21 +0000

    mergelol

--- ferrets/static/js/zoom.js
+++ ferrets/static/js/zoom.js
@@ -2,16 +2,12 @@ $(document).ready(function(){
 	$('#zoom').hover(function(){
 		console.log("HI");
 		var scale = 150/100;
-		$(this).css({
-			width: this.width*scale,
-			height: this.height*scale
-		})
+		$(this).animate({width : width * scale}, 20);
+		$(this).animate({height: height * scale}, 20);
 }, 
 	function(){
 	var scale = 150/100;
-		$(this).css({
-			width: this.width/scale,
-			height: this.height/scale
-		})
+		$(this).animate({"width" : width / scale}, 20);
+		$(this).animate({"height": height / scale}, 20);
 	});
 });
