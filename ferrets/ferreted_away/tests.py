7b689d59297420823f3ea0da1b4a4947125ef241
KerryJohnstone <2189152j@student.gla.ac.uk>
2017-03-24 15:23:46 +0000

    Setting up live server tests

--- ferrets/ferreted_away/tests.py
+++ ferrets/ferreted_away/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from ferreted_away.models import *
 from django.urls import reverse
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.Chrome.webdriver import WebDriver
 
 class ModelTests(TestCase):
 	def setUp(self):
@@ -174,6 +176,27 @@ class ViewsTests(TestCase):
 		response = self.client.get(reverse('verify'))
 		self.assertEquals(response.status_code, 200)
 		
+class MySeleniumTests(StaticLiveServerTestCase):
+	fixtures = ['user_data.json']
+	
+	@classmethod
+	def setUpClass(cls):
+		super(MySeleniumTests, cls).setUpClass()
+		cls.selenium = WebDriver()
+		cls.selenium.implicilty_wait(10)
+	
+	@classmethod
+	def tearDownClass(cls):
+		cls.selenium.quit()
+		super(MySeleniumTests, cls).tearDownClass()
+		
+	def test_login(self):
+		self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+		username_input = self.selenium.find_element_by_name("username")
+		username_input.send_keys('myuser')
+		password_input = self.selenium.find_element_by_name("password")
+		password_input.send_keys('secret')
+		self.selenium.find_element_by_xpath('//input[@value="Log In"]').click()
 	
 		
 	
