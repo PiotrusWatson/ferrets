7fe6ee79f2f89011434a247f17112df0bd2d7a48
KerryJohnstone <2189152j@student.gla.ac.uk>
2017-03-24 11:30:21 +0000

    We have 4 whole test cases

--- ferrets/ferreted_away/tests.py
+++ ferrets/ferreted_away/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from ferreted_away.models import *
+"""
+https://github.com/leifos/tango_with_django_19/blob/master/code/tango_with_django_project/rango/tests.py
+"""
 
-# Create your tests here.
+class ModelTests(TestCase):
+	def setUp(self):
+		try:
+			from population_script import populate
+			populate()
+		except ImportError:
+			print('The module population_script does not exist')
+		except NameError:
+			print('The function populate() does not exist')
+		except:
+			print('Undisclosed error')
+
+	def get_category(self, name):
+		from ferreted_away.models import Category
+		try:
+			cat = Category.objects.get(name=name)
+		except Category.DoesNotExist:
+			cat = None
+		return cat
+		
+	def test_if_category_added(self):
+		cat = self.get_category('Transport')
+		self.assertIsNotNone(cat)
+		
+	def get_item(self, name):
+		from ferreted_away.models import Item
+		try:
+			item = Item.objects.get(item_name=name)
+		except Item.DoesNotExist:
+			item = None
+		return item
+	
+	def test_if_item_added(self):
+		item = self.get_item('Rollerskates')
+		self.assertIsNotNone(item)
+		
+	def test_if_item_has_views(self):
+		item = self.get_item('Rollerskates')
+		item.views = 56
+		item.save()
+		self.assertEquals(item.views, 56)
+		
+	def get_user(self, name):
+		from django.contrib.auth.models import User
+		try:
+			user = User.objects.get(username=name)
+		except User.DoesNotExist:
+			user = None
+		return user
+		
+	def test_is_user_added(self):
+		user = self.get_user('fredrick')
+		self.assertIsNotNone(user)
+			
--- ferrets/static/js/zoom.js
+++ ferrets/static/js/zoom.js
@@ -1,32 +1,29 @@
 $(document).ready(function(){
-	var curWidth=$('#zoom').css('width');
-	var curHeight=$('#zoom').css('height');
-	var biganim=false;
-	var smallanim=false;
+	var curWidth=$('#zoom').css('width'); //gets default width of image
+	var curHeight=$('#zoom').css('height'); //gets default height of image
+	var biganim=false; //safety for making bigger
+	var smallanim=false; //safety for making smaller
 	
 	$('#zoom').hover(function(event){
-		if (biganim) return false;
-		console.log("HI");
+		//while mouse is over image, make it bigger
+		if (biganim) return false; //don't load again if already animating
 		var scale = 150/100;
-		biganim=true;
+		biganim=true; //we are animating right now!
 		$('#zoom').animate({width: this.width*scale,
-			height: this.height*scale},
+			height: this.height*scale}, //make image bigger, but real slow
 			"slow",
 			"swing", 
-			function(){ biganim=false;});
-			
+			function(){ biganim=false;}); //once animation is over, tell boolean we've stopped :)
+	
 }, 
 	function(event){
-		if (smallanim) return false;
-	var scale = 150/100;
+		//while mouse is off image, make it its default size
+		if (smallanim) return false; //don't do this again if already animating
 	smallanim=true;
-	console.log(curWidth);
-	console.log(curHeight);
-	$('#zoom').animate({width: curWidth,
+	$('#zoom').animate({width: curWidth, //animates size down to original width/height, but real slow
 			height: curHeight},
 			"slow",
 			"swing",
-			function(){ smallanim=false; });
-			
+			function(){ smallanim=false; }); //once animation is over, tell boolean we've stopped 
 	});
 });
